ed9f24aa4e91d442ab15195db7f7ad43b259b983
itherdevs <iisistiharoh59@gmail.com>
2021-03-05 22:23:37 +0700

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.master')
+
+@section('title', 'laravel')
+@section('content')
+<div class="section-body">
+     
+     {{-- Dashboard atas --}}
+
+   
+
+</div>
+@endsection
+
+@push('page-scripts')
+    
+@endpush
---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,41 @@
+<div class="main-sidebar">
+    <aside id="sidebar-wrapper">
+      <div class="sidebar-brand">
+        <a href="#">E-LEARNING AKADEMIK</a>
+      </div>
+      <div class="sidebar-brand sidebar-brand-sm">
+        <a href="#">GL</a>
+      </div>
+        <ul class="sidebar-menu">
+          <li class="menu-header">Home</li>
+          <li class="">
+            <a href="#" class="nav-link"><i class="fas fa-fire"></i><span>Home</span></a>
+          </li>
+          <li class="#">
+            <a href="#" class="nav-link"><i class="fas fa-folder-open"></i><span>Course</span></a>
+          </li>
+          <li class="#">
+            <a href="#" class="nav-link"><i class="fas fa-folder-open"></i><span>Match Question</span></a>
+          </li>
+          <li class="#">
+            <a href="#" class="nav-link"><i class="fas fa-folder-open"></i><span>Event</span></a>
+          </li>
+          <li class="#">
+            <a href="#" class="nav-link"><i class="fas fa-folder-open"></i><span>Achievement</span></a>
+          </li>
+          <li class="#">
+            <a href="#" class="nav-link"><i class="fas fa-folder-open"></i><span>Leaderboard</span></a>
+          </li>
+          <li class="#">
+            <a href="#" class="nav-link"><i class="fas fa-folder-open"></i><span>FaQ</span></a>
+          </li>
+          
+        </ul>
+
+        {{-- <div class="mt-4 mb-4 p-3 hide-sidebar-mini">
+          <a href="https://getstisla.com/docs" class="btn btn-primary btn-lg btn-block btn-icon-split">
+            <i class="fas fa-rocket"></i> Documentation
+          </a>
+        </div> --}}
+    </aside>
+  </div>
